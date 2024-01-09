43d497d58f0c9353520513895181316eb71994de
Malik H <malik.h@webdevpro.net>
2024-01-09 15:24:02 +0100

    note de cours

--- jour2/05-decouverte.php
+++ jour2/05-decouverte.php
@@ -35,5 +35,81 @@ echo $nom ; // console.log()
 // => appeler le serveur 
 // http://192.168.15.15/05-decouverte.php
 
+// en PHP il est possible stocker dans une variable
+// plusieurs type de valeurs
+
+// string / chaine de caractères
+$texte = "du texte" ; // guillemet
+$texte = 'encore du texte' ; // apostrophes 
+// `` backtick ne sont pas disponibles en PHP  
+
+// vous pouvez stocker des chiffres dans des variables 
+// en PHP on distingue chiffre entier => integer et les chiffres à virgule => float
+
+$distance = 200 ; // int
+$prix = 10.3 ;    // float
+$prixVoiture = 10_000 ; // il est possible d'ajouter le symbole _ (underscore ) pour aérer votre chiffre 
+// chiffre qui n'a aucun impact sur le valeur de 10000 
+
+// boolean
+$isAdmin = true ; 
+$toto = false ; 
+
+// attention il ne faut pas confondre 
+$test1 = true ;
+$test2 = "true" ;
+
+$false = 0 ;  // false 
+$vrai = 22 ; // true 
+
+$autreTest = TRUE ;
+$autreTest2 = True ; // je 
+
+// variable simple = 1 variable contient 1 valeur 
+
+// variable complexe => tableau (array) // objet 
+
+// en js il n'existe 1 seul type de tableau
+// const a = [ 1,2,3 ]; 
+
+// en PHP il existe 2 types de tableaux 
+
+// tableaux indexés 
+
+$etudiants = ["Alain" , "Celine" , "Zorro" ]; 
+$etudiants=["Alain","Celine","Zorro"]; 
+$etudiants= [
+        "Alain",  // 0
+        "Celine", // 1
+        "Zorro"   // 2
+]; 
+// chaque valeur stockée dans le tableau est accessible via sa position (index)
+// exactement la même chose que sur le langage javascript 
+
+// deuxième et dernier type de tableau tableaux associatifs 
+
+$formation = [
+    "duree" => 10 ,
+    "prix"  => 5000 ,
+    "adresse" => "75 rue de Paris",
+    "isOpen" => false
+    // key   => valeur
+];
+// le tableau formation contient 4 valeurs 
+// spéciale PHP n'existe pas en javascript 
+
+
+// en javascript et PHP il est possible de stocker dans une variable 
+// des objets 
+// const a = { prop : "valeur" };
+
+// en PHP pour créer un objet il faut OBLIGATOIRE et AU PREALABLE avec crée une class 
+
+class Etudiant {} // je crée une class
+
+// et à ce moment là je peux créer un objet etudiant
+
+$etudiant = new Etudiant(); 
+
 ?>
 
