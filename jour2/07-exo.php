2c278208c3ec14c47072b41032c549999f4a26d7
Malik H <malik.h@webdevpro.net>
2024-01-09 16:57:02 +0100

    fin journée

---
+++ jour2/07-exo.php
@@ -0,0 +1,12 @@
+<?php 
+
+$unite = "euros" ;
+$montant = 30 ;
+$objet = "dejeuner au restaurant" ; 
+
+echo "j'ai dépensé $montant $unite <br>" ;
+echo "mon $objet est prévu à 11h$montant";
+
+// http://192.168.15.15/07-exo.php
+
+// 
