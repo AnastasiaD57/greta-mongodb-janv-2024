d399c807a7ce337c893616d1f9494864ce850b08
Malik H <malik.h@webdevpro.net>
2024-01-11 16:56:52 +0100

    correction

---
+++ jour2/21-exo.php
@@ -0,0 +1,18 @@
+<?php 
+
+// http://192.168.15.15/21-exo.php
+
+function aireCercle( float $rayon ) : void{
+    $resultat = 3.14 * $rayon * $rayon ; 
+    echo "l'aire d'un cercle de rayon $rayon a une aire de $resultat <br>" ; 
+}
+
+aireCercle(44.5);
+aireCercle(70.43);
+aireCercle(70.43);
+aireCercle(70.43);
+aireCercle(70.43);
+aireCercle(70.43);
+aireCercle(70.43);
+aireCercle(70.43);
+aireCercle(70.43);
