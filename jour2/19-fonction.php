f5073d4864f0021944ec4eeb498fa033bd5c858c
Malik H <malik.h@webdevpro.net>
2024-01-11 15:30:40 +0100

    fonction part 1

---
+++ jour2/19-fonction.php
@@ -0,0 +1,19 @@
+<?php 
+
+// http://192.168.15.15/19-fonction.php
+
+// fonction permet de stocker des instructions 
+// une variable qui contient des instructions 
+
+// déclarer une fonction => stockage 
+// créer une variable premier ET on vient de stocker dedans 3 instructions
+function premier (){
+    $a = 1 ;
+    $b = 2 ;
+    echo $a + $b ;
+}
+// dans premier vous stockez 3 instructions 
+premier() ; // Warning: Undefined variable $premier
+return $a + $b;
+
+//premier (); 
