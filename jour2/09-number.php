d34bd570ea77c828547003781a91396a5513783b
Malik H <malik.h@webdevpro.net>
2024-01-11 10:32:53 +0100

    nombre en PHP

---
+++ jour2/09-number.php
@@ -0,0 +1,21 @@
+<?php 
+// http://192.168.15.15/09-number.php
+
+$a = 1 ;
+$b = 2 ; 
+
+// addition / soustraction / division / multiplication
+echo $a + $b . "<br>"; // addition
+echo $a - $b . "<br>"; // soustraction
+echo $a / $b . "<br>"; // division
+echo $a * $b . "<br>"; // multiplication
+
+// puissance / modulo
+$c = 3 ;
+$d = 4 ;
+echo $c ** $d . "<br>" ; //puissance
+echo $d % $b . "<br>" ; // modulo => reste entier d'une division
+echo $c % $b . "<br>"; // modulo => reste entier d'une division
+
+// 4 / 2 => 2 reste entier 0
+// 3 / 2 => 1 reste entier 1
