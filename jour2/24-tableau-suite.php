22f44f8b8564fc10982b8f19d77dafdc335b667a
Malik H <malik.h@webdevpro.net>
2024-01-12 10:31:15 +0100

    @ toute suite

---
+++ jour2/24-tableau-suite.php
@@ -0,0 +1,12 @@
+<?php 
+
+// http://192.168.15.15/24-tableau-suite.php
+
+$premierTableau = []; 
+
+$premierTableau["age"] = 10 ;
+$premierTableau["adresse"] = "30 rue de Paris";
+$premierTableau["isAdmin"] = true ;
+$premierTableau["prix"] = 22.5 ; 
+
+var_dump($premierTableau); 
