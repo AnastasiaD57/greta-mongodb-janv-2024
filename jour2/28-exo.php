493d1670a781ee93534cb04aa172bcaebbb8ec44
Malik H <malik.h@webdevpro.net>
2024-01-12 13:46:23 +0100

    cas pratique

--- jour2/28-exo.php
+++ jour2/28-exo.php
@@ -25,3 +25,33 @@ for($i = 0 ; $i <= count($fleurs) - 1 ; $i++){
 }
 
 // PDO => PHP Data Object 
+
+// créer le fichier 29-exo.php
+// dans ce fichier vous allez créer un tableau multidimention
+
+/*
+articles qui est un tableau qui contient 3 valeurs
+et chaque valeur de ce tableau est elle même un tableau
+
+titre : "article 1" / contenu : "lorem ipsum 1" / img : https://via.placeholder.com/400x200
+titre : "article 2" / contenu : "lorem ipsum 2" / img : https://via.placeholder.com/400x200
+titre : "article 3" / contenu : "lorem ipsum 3" / img : https://via.placeholder.com/400x200
+
+// via une boucle qui affiche à l'écran le texte suivant (texte qui contient du html)
+<article>
+    <h2>article 1</h2>
+    <img src="https://via.placeholder.com/400x200" alt="">
+    <p>lorem ipsum 1</p>
+</article>
+<article>
+    <h2>article 2</h2>
+    <img src="https://via.placeholder.com/400x200" alt="">
+    <p>lorem ipsum 2</p>
+</article>
+<article>
+    <h2>article 3</h2>
+    <img src="https://via.placeholder.com/400x200" alt="">
+    <p>lorem ipsum 3</p>
+</article>
+*/ 
+
