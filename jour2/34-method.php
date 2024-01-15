f16da1c9eb3389b1d26834ef9016a9c39cec988d
Malik H <malik.h@webdevpro.net>
2024-01-15 10:59:13 +0100

    cas pratique

--- jour2/34-method.php
+++ jour2/34-method.php
@@ -15,3 +15,17 @@ class Carre {
 // une fois que la class est créée, si on veut exécuter la méthode perimetre 
 $carre = new Carre();
 $carre->perimetre(); // exécuter la méthode perimetre stockée dans l'objet $carre qui est définie dans la class Carre  
+
+// cas pratique 
+// créer le fichier 35-exo.php
+// dans ce fichier vous allez créer une class qui s'appelle Etudiant
+// cette class contient 2 méthodes
+
+// getNom 
+// cette méthode réalisé un echo du texte "je m'appelle Alain"
+
+// getAdresse
+// cette méthode return le texte suivant "75 rue de Paris";
+
+// une fois la class créée, exécuter les deux méthodes est afficher leur résultat dans le navigateur
+
